8c9c4c07522d4750c479bd663ab2c1a4a53a35f3
jvazquez2028 <jvazquez2028@gmail.com>
2022-02-26 14:24:36 -0600

    updates to js format

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ function toggleDarkMode() {
 
 //if in dark mode, change to light
 
-if (bodyElement.style.background === "black") {
-    bodyElement.style.background = "rgb(255, 255, 254)";
-    bodyElement.style.color ="black";
-    darkModeButtonElement.innerText = "Dark Mode";
-} else {
-    bodyElement.style.background ="black";
-    bodyElement.style.color ="white";
-    darkModeButtonElement.innerHTML = "Light Mode"
-}
-
+    if (bodyElement.style.background === "black") {
+        bodyElement.style.background = "rgb(255, 255, 254)";
+        bodyElement.style.color ="black";
+        darkModeButtonElement.innerText = "Dark Mode";
+    } else {
+        bodyElement.style.background ="black";
+        bodyElement.style.color ="white";
+        darkModeButtonElement.innerHTML = "Light Mode"
+    }
 }
