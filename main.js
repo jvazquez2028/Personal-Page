2e08712468fe740ba3b5cd188c5100b4310ce29b
jvazquez2028 <jvazquez2028@gmail.com>
2022-03-05 13:06:19 -0600

    added media queries

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
+
 function toggleDarkMode() {
     let bodyElement = document.getElementById('main-background');
     let darkModeButtonElement = document.getElementById('darkMode');
+    let navbarHeaderElement = document.getElementById('navbar');
 
 //if in dark mode, change to light
 
@@ -8,9 +16,12 @@ function toggleDarkMode() {
         bodyElement.style.background = "rgb(255, 255, 254)";
         bodyElement.style.color ="black";
         darkModeButtonElement.innerText = "Dark Mode";
+        navbarHeaderElement.style.background = "white";
     } else {
         bodyElement.style.background ="black";
         bodyElement.style.color ="white";
+        navbarHeaderElement.style.background = "black";
         darkModeButtonElement.innerHTML = "Light Mode"
+        
     }
 }
